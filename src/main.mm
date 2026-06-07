96ef1bb9950877d36994874954f0edb33f9271de
Senthil Shelke <senthil.shelke@gmail.com>
2026-06-07 18:27:47 -0400

    hookup metal devices to dot product

--- src/main.mm
+++ src/main.mm
@@ -44,7 +44,7 @@ Matrix create_matrix(string file) {
 int main() {
 
     Matrix db = create_matrix("data/vector_db.bin");
-    Matrix query = create_matrix("data/query.bin");
+    Matrix query = create_matrix("data/vector_db.bin");
     int db_bytes = db.data.size() * sizeof(float);
     int query_bytes = query.data.size() * sizeof(float);
     int result_bytes = db.rows * sizeof(float);
@@ -54,6 +54,36 @@ int main() {
     id<MTLBuffer> db_buffer = [device newBufferWithBytes:db.data.data() length:db_bytes options:MTLResourceStorageModeShared];
     id<MTLBuffer> query_buffer = [device newBufferWithBytes:query.data.data() length:query_bytes options:MTLResourceStorageModeShared];
     id<MTLBuffer> result_buffer = [device newBufferWithLength:result_bytes options:MTLResourceStorageModeShared];
+
+    id<MTLLibrary> library = [device newDefaultLibrary];
+    id<MTLFunction> dot_product = [library newFunctionWithName:@"dot_product"];
+    NSError* error = nil;
+    id<MTLComputePipelineState> pipeline_state = [device newComputePipelineStateWithFunction:dot_product error:&error];
+    if(!pipeline_state) {
+        cout << "Failed to create pipeline state." << endl;
+        exit(1);
+    }
+
+    id<MTLCommandBuffer> command_buffer = [command_queue commandBuffer];
+    id<MTLComputeCommandEncoder> encoder = [command_buffer computeCommandEncoder];
+
+    [encoder setComputePipelineState:pipeline_state];
+    [encoder setBuffer:db_buffer offset:0 atIndex:0];
+    [encoder setBuffer:query_buffer offset:0 atIndex:1];
+    [encoder setBuffer:result_buffer offset:0 atIndex:2];
+
+    MTLSize grid_size = MTLSizeMake(db.rows, 1, 1);
+    [encoder dispatchThreads:grid_size threadsPerThreadgroup:MTLSizeMake(64, 1, 1)];
+    [encoder endEncoding];
+    [command_buffer commit];
+    [command_buffer waitUntilCompleted];
+
+    float* results = (float*)result_buffer.contents;
+    for(int i = 0; i < 5; i++) {
+        cout << results[i] << endl;
+    } 
+
+
 
     return 0;
 }
